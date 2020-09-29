aafe56a1cc95b06b7b17fa00ac823dd0a4115c0a
kimjunwoo <junwoo1027@naver.com>
2020-09-29 20:30:25 +0900

    회원 가입 서브밋 검증 추가

--- src/main/java/com/studyolle/account/AccountController.java
+++ src/main/java/com/studyolle/account/AccountController.java
@@ -1,16 +1,42 @@
 package com.studyolle.account;
 
+import com.studyolle.domain.Account;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
 
 @Controller
+@RequiredArgsConstructor
 public class AccountController {
+
+    private final SignupFormValidator signupFormValidator;
+    private final AccountRepository accountRepository;
+
+    @InitBinder("signUPForm")
+    public void initBinder(WebDataBinder webDataBinder) {
+        webDataBinder.addValidators(signupFormValidator);
+    }
 
     @GetMapping("/sign-up")
     public String sighUpForm(Model model) {
         //model.addAttribute("signUpForm", new SignUpForm());
-        model.addAttribute(new SignUpForm()); //클래스의 이름으로 값을 카멜이스로 사용
+        model.addAttribute(new SignUpForm()); //클래스의 이름으로 값을 카멜케이스로 사용
         return "account/sign-up";
+    }
+
+    @PostMapping("/sign-up")
+    public String signUpSubmit(@Valid SignUpForm signUpForm, Errors errors) {
+        if (errors.hasErrors()) {
+            return "account/sign-up";
+        }
+
+        return "redirect:/";
     }
 }
