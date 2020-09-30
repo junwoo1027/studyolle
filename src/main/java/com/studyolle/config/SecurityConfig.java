5d9b043634b417537de21deb145e065337def89f
junwoo1027 <junwoo1027@naver.com>
2020-09-30 14:24:40 +0900

    프론트엔드 라이브러리 설정

--- src/main/java/com/studyolle/config/SecurityConfig.java
+++ src/main/java/com/studyolle/config/SecurityConfig.java
@@ -23,6 +23,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     public void configure(WebSecurity web) throws Exception { //static resource security filter 무시
         web.ignoring()
+                .mvcMatchers("/node_modules/**")
                 .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
     }
 }
