fb20c9427a330a63e6c06fdf8319272bbcb95279
junwoo1027 <junwoo1027@naver.com>
2020-10-05 20:29:56 +0900

    스터디 설정 - 경로 및 이름 수정

--- src/main/java/com/studyolle/study/StudyService.java
+++ src/main/java/com/studyolle/study/StudyService.java
@@ -11,6 +11,8 @@ import org.springframework.security.access.AccessDeniedException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import static com.studyolle.study.form.StudyForm.VALID_PATH_PATTERN;
+
 @Service
 @Transactional
 @RequiredArgsConstructor
@@ -121,5 +123,24 @@ public class StudyService {
     public void closeAndStopRecruit(Study study) {
         study.close();
         study.stopRecruit();
+    }
+
+    public boolean isValidPath(String newPath) {
+        if (!newPath.matches(VALID_PATH_PATTERN)) {
+            return false;
+        }
+        return !studyRepository.existsByPath(newPath);
+    }
+
+    public void updateStudyPath(Study study, String newPath) {
+        study.setPath(newPath);
+    }
+
+    public boolean isValidTitle(String newTitle) {
+        return newTitle.length() <= 50;
+    }
+
+    public void updateStudyTitle(Study study, String newTitle) {
+        study.setTitle(newTitle);
     }
 }
