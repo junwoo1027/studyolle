d5b2bfce2b9f3ce50102e58a6c6c22a357f685c7
junwoo1027 <junwoo1027@naver.com>
2020-10-05 23:06:44 +0900

    스터디 소개 수정 및 가입,탈퇴 테스트

--- src/test/java/com/studyolle/study/StudyControllerTest.java
+++ src/test/java/com/studyolle/study/StudyControllerTest.java
@@ -5,6 +5,7 @@ import com.studyolle.account.AccountRepository;
 import com.studyolle.domain.Account;
 import com.studyolle.domain.Study;
 import lombok.RequiredArgsConstructor;
+import org.checkerframework.checker.units.qual.A;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -26,15 +27,15 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @RequiredArgsConstructor
 class StudyControllerTest {
 
-    @Autowired MockMvc mockMvc;
-    @Autowired StudyService studyService;
-    @Autowired StudyRepository studyRepository;
-    @Autowired AccountRepository accountRepository;
+    @Autowired protected MockMvc mockMvc;
+    @Autowired protected StudyService studyService;
+    @Autowired protected StudyRepository studyRepository;
+    @Autowired protected AccountRepository accountRepository;
 
-    @AfterEach
-    void afterEach() {
-        accountRepository.deleteAll();
-    }
+//    @AfterEach
+//    void afterEach() {
+//        accountRepository.deleteAll();
+//    }
 
     @Test
     @WithAccount("junwoo1027")
@@ -103,5 +104,52 @@ class StudyControllerTest {
                 .andExpect(view().name("study/view"))
                 .andExpect(model().attributeExists("account"))
                 .andExpect(model().attributeExists("study"));
+    }
+
+    @Test
+    @DisplayName("스터디 가입")
+    @WithAccount("junwoo")
+    void joinStudy() throws Exception {
+        Account junwoo1027 = createAccount("junwoo1027");
+        Study study = createStudy("test-study", junwoo1027);
+
+        mockMvc.perform(get("/study/" + study.getPath() + "/join"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/study/" + study.getPath() + "/members"));
+
+        Account account = accountRepository.findByNickname("junwoo");
+        assertTrue(study.getMembers().contains(account));
+    }
+
+    @Test
+    @DisplayName("스터디 탈퇴")
+    @WithAccount("junwoo")
+    void leaveStudy() throws Exception {
+        Account junwoo1027 = createAccount("junwoo1027");
+        Study study = createStudy("test-study", junwoo1027);
+
+        Account account = accountRepository.findByNickname("junwoo");
+        studyService.addMember(study, account);
+
+        mockMvc.perform(get("/study/" + study.getPath() + "/leave"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/study/" + study.getPath() + "/members"));
+
+        assertFalse(study.getMembers().contains(account));
+    }
+
+    protected Study createStudy(String path, Account manager) {
+        Study study = new Study();
+        study.setPath(path);
+        studyService.createNewStudy(study, manager);
+        return study;
+    }
+
+    protected Account createAccount(String nickname) {
+        Account account = new Account();
+        account.setNickname(nickname);
+        account.setEmail(nickname + "@naver.com");
+        accountRepository.save(account);
+        return account;
     }
 }
